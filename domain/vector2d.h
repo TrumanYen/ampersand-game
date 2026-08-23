38ee3e38773ba6ce68bd8d2d747fd1d6aeeabde5
Truman Yen <yen.truman@gmail.com>
2026-08-22 20:16:08 -0600

    Make vector2d overloads const
    so that you can use them on const vectors.

--- domain/vector2d.h
+++ domain/vector2d.h
@@ -10,23 +10,23 @@ public:
   Vector2D(T x, T y) : x(x), y(y) {}
   ~Vector2D() = default;
 
-  Vector2D<T> operator+(Vector2D<T> rhs) {
+  Vector2D<T> operator+(Vector2D<T> rhs) const {
     return Vector2D<T>(x + rhs.x, y + rhs.y);
   }
 
-  Vector2D<T> operator-(Vector2D<T> rhs) {
+  Vector2D<T> operator-(Vector2D<T> rhs) const {
     return Vector2D<T>(x - rhs.x, y - rhs.y);
   }
 
-  Vector2D<T> operator/(T scalar) {
+  Vector2D<T> operator/(T scalar) const {
     return Vector2D<T>(x / scalar, y / scalar);
   }
 
-  Vector2D<T> operator*(T scalar) {
+  Vector2D<T> operator*(T scalar) const {
     return Vector2D<T>(x * scalar, y * scalar);
   }
 
-  T squaredDistanceFrom(Vector2D<T> other) {
+  T squaredDistanceFrom(Vector2D<T> other) const {
     Vector2D<T> displacement = (*this) - other;
     return (displacement.x * displacement.x) +
            (displacement.y * displacement.y);
